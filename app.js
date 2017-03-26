af6d2b23a3790ead1cc2faa0e1f26c0e293f4cf8
MOHAB <mohabamr1@gmail.com>
2017-03-26 23:31:00 +0200

    heroku db fix

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var LocalStrategy = require('passport-local').Strategy;
 var userUploadsPath = path.resolve(__dirname, "user_uploads");
 var publicPath = path.join(__dirname, 'public');
 var mongo = require('mongodb');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://mohabamroo:ghostrider1@ds127260.mlab.com:27260/communitydb');
-mongoose.connect('mongodb://localhost/communitydb');
+// mongoose.connect('mongodb://localhost/communitydb');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
